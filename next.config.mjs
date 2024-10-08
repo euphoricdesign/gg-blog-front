bbd4ef3f9af493cded8fd5aea8782a625a2369aa
Merlina <noahchukita@gmail.com>
2024-10-08 17:21:47 -0300

    chore(config): actualiza configuración de Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['assets.glamour.de', 'static.wikia.nocookie.net', 'i.pinimg.com', 'uwagg6ytf2u3hnso.public.blob.vercel-storage.com'],
+    }
+};
 
 export default nextConfig;
